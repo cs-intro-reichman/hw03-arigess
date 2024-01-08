cba60f88cd78843d714b35b4de728954e4a81c6d
אריאל גס <arielgess@gmail.com>
2024-01-08 19:49:40 +0200

    Now receiving real input for calendar0

--- Calendar0.java
+++ Calendar0.java
@@ -6,8 +6,7 @@ public class Calendar0 {
 
     // Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
     public static void main(String args[]) {
-        //int year = Integer.parseInt(args[0]);
-        int year = 2020;
+        int year = Integer.parseInt(args[0]);
         isLeapYearTest(year);
         nDaysInMonthTest(year);
     }
